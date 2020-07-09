b44887484c7118e180709f02ebc2826c218a7383
chrisveja1103 <chrisveja28@gmail.com>
2020-07-09 10:04:12 -0400

    New commits

--- fcns - Copy/working.jl
+++ fcns - Copy/working.jl
@@ -9,7 +9,7 @@
 using Random, Distributions,Statistics, LinearAlgebra,
 	Plots,StatsBase,Parameters, Flux;
 include("DefaultEconomy.jl");
-include("fafafa.jl");
+include("convergenceNN.jl");
 include("graphs.jl");
 
 ############################################################
@@ -19,38 +19,36 @@ include("graphs.jl");
 ############################################################
 EconDef = DefaultEconomy.ModelSettings();
 EconSol = DefaultEconomy.SolveDefEcon(EconDef);
-#graph_solve(EconSol);
+graph_solve(EconSol);
 
 ############################################################
 #	[Simulation]
 ############################################################
 tsim    = 10000; tburn   = 0.05;
 EconSim = DefaultEconomy.ModelSimulate(EconSol,nsim=tsim,burn=tburn);
-#graph_simul(EconSim, smpl=1:500);
+graph_simul(EconSim, smpl=1:500);
 
 ############################################################
 #	[Neural Network]
 ############################################################
 VFNeuF  = (vf= EconSim.Sim[:,6], q = EconSim.Sim[:,7],states= EconSim.Sim[:,2:3]);
 VFhat   = DefaultEconomy.NeuralTraining(VFNeuF[1],VFNeuF[3], Nepoch = 10);
-#graph_neural(VFhat,"Value Function", ["VFneural.png" "VFSmpl.png"],smpl=1:500);
+graph_neural(VFhat,"Value Function", ["VFneural.png" "VFSmpl.png"],smpl=1:500);
 
 ############################################################
 # [2] Solving - simulating - training
 ############################################################
 convergenceNN(EconSol,VFhat, tburn);
-
 EconSol1, EconSim1, mytup = convergenceNN(EconSol,VFhat, tburn);
-
-
+graph_solve(EconSol1,titles=["BondPrice1.png" "Savings1.png" "ValFun1.png"]);
 
 MatVal1 = cat(EconSol.Sol.ValNoD, mytup.vc₀, EconSol1.Sol.ValNoD, dims=3);
 MatVal2 = cat(EconSol.Sol.ValDef, mytup.vd₀, EconSol1.Sol.ValDef, dims=3);
 
-```
-[Graphics]
-	[∘] some graphics less standard
-```
+############################################################
+#[Graphics]
+#	[∘] some graphics less standard
+############################################################
 _auxgraph(MatVal1,1:9,".//Figures//CompVC1");
 _auxgraph(MatVal1,10:18,".//Figures//CompVC2");
 _auxgraph(MatVal1,19:21,".//Figures//CompVC3");
